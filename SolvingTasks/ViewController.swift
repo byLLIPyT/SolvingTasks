44d7667279617ac9297c9a1755c9ca22fdc0771f
byLLIPyT <byllipyt@icloud.com>
2021-05-21 22:06:27 +0300

    Initial Commit

---
+++ SolvingTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolvingTasks
+//
+//  Created by Александр Уткин on 21.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
